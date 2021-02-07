9adb6ee17aeb60ef9d863c8f08d5f85926528f81
timaaos <timaaos@users.noreply.github.com>
2021-02-07 22:05:21 +0300

    cleared from trash

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
 import api as mc
 import pyglet
-def main():
-    window = mc.Window(width=800, height=600, caption='CraftPy', resizable=True)
-
-
-
-
-    mc.setup()
-    window.MIN_Y = 0
-    pyglet.app.run()
-if __name__ == '__main__':
-    main()
+window = mc.Window(width=800, height=600, caption='CraftPy', resizable=True)
+mc.setup()
+pyglet.app.run()
